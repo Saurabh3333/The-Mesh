abcc7a8faad72493d46a7cc87f595130fddf30bd
Saurabh Shubham <saurabh.friday@gmail.com>
2017-12-14 14:08:53 +0530

    sign up page design ready

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 const express = require('express');
-
+var path = require('path');
 
 var app = express();
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+   res.render('signup');
+});
+
+app.listen(8080, () => {
+    console.log(" The Mesh is now running at http://localhost:8080 ");
+  });
